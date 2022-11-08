5874fcaf885233ff5e8d9febd6bbc0b74b9e4977
sharmin1988 <rasnasharmin11@gmail.com>
2022-11-08 16:20:35 +0600

    home page service card add

---
+++ src/Page/Home/ServicesInHomePage/HomepageServiceCard.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const HomepageServiceCard = ({ service }) => {
+    const { title, description, image, price } = service
+    return (
+        <div>
+            <div className="flex w-full">
+                <div className="relative flex flex-col items-start m-1 transition duration-300 ease-in-out delay-150 transform bg-white shadow-2xl rounded-xl md:w-80 md:-ml-16 md:hover:-translate-x-16 md:hover:-translate-y-8">
+                    <img className="object-cover object-center w-full rounded-t-xl lg:h-48 md:h-36" src={image} alt="blog" />
+                    <div className="px-6 py-8">
+                        <h4 className="mt-4 text-2xl font-semibold text-neutral-600">
+                            <span className="">{title}</span>
+                        </h4>
+                        <p className="mt-4 text-base font-normal text-gray-500 leading-relax"
+                        >Description: ${description.length > 100 ? description.slice(0, 100) + '...' : description}
+                        </p>
+                        <p className="mt-4 text-base font-normal text-gray-500 leading-relax"
+                        >Course Fee: ${price}
+                        </p>
+                        <Link to='/services'><button className="w-full tracking-wider px-4 font-semibold py-2.5 mt-6 text-sm text-white transition-colors duration-300 transform bg-orange-600 rounded-md lg:w-auto hover:bg-purple-500 focus:outline-none focus:bg-purple-500">More details</button></Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default HomepageServiceCard;
--- src/Page/Home/ServicesInHomePage/ServicesInHomePage.js
+++ src/Page/Home/ServicesInHomePage/ServicesInHomePage.js
@@ -1,66 +1,37 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import HomepageServiceCard from './HomepageServiceCard';
 
 const ServicesInHomePage = () => {
-    const { services, setServices } = useState([])
+    const [services, setServices] = useState([])
 
     useEffect(() => {
         fetch('products.json')
             .then(res => res.json())
-            .then(data => console.log(data))
+            .then(data => {
+                console.log(data)
+                setServices(data)
+            })
     })
 
     return (
         <section class="my-4 container px-6 py-10 mx-auto">
-                <h2 className='text-3xl text-center font-bold mb-6'>My services</h2>
-                <div class="flex flex-wrap mx-auto md:flex-nowrap w-3/4">
-
-                    <a href="">
-                        <div class="flex w-full">
-                            <div class="relative flex flex-col items-start m-1 transition duration-300 ease-in-out delay-150 transform bg-white shadow-2xl rounded-xl md:w-80 md:-ml-16 md:hover:-translate-x-16 md:hover:-translate-y-8">
-                                <img class="object-cover object-center w-full rounded-t-xl lg:h-48 md:h-36" src="/assets/images/placeholders/neon-1.jpg" alt="blog"/>
-                                    <div class="px-6 py-8">
-                                        <h4 class="mt-4 text-2xl font-semibold text-neutral-600">
-                                            <span class="">Entry</span>
-                                        </h4>
-                                        <p class="mt-4 text-base font-normal text-gray-500 leading-relax">Install Tailwind CSS without any Javascript Framewrok locally with purgeCSS, enable the dark mode option, prefferences or class is upt to you.</p>
-                                    </div>
-                            </div>
-                        </div>
-                    </a>
-
-                    <a href="">
-                        <div class="flex w-full">
-                            <div class="relative flex flex-col items-start m-1 transition duration-300 ease-in-out delay-150 transform bg-white shadow-2xl rounded-xl md:w-80 md:-ml-16 md:hover:-translate-x-16 md:hover:-translate-y-8">
-                                <img class="object-cover object-center w-full rounded-t-xl lg:h-48 md:h-36" src="/assets/images/placeholders/neon-4.jpg" alt="blog"/>
-                                    <div class="px-6 py-8">
-                                        <h4 class="mt-4 text-2xl font-semibold text-neutral-600">
-                                            <span class="">Entry</span>
-                                        </h4>
-                                        <p class="mt-4 text-base font-normal text-gray-500 leading-relax">Install Tailwind CSS without any Javascript Framewrok locally with purgeCSS, enable the dark mode option, prefferences or class is upt to you.</p>
-                                    </div>
-                            </div>
-                        </div>
-                    </a>
-
-                    <a href="">
-                        <div class="flex w-full">
-                            <div class="relative flex flex-col items-start m-1 transition duration-300 ease-in-out delay-150 transform bg-white shadow-2xl rounded-xl md:w-80 md:-ml-16 md:hover:-translate-x-16 md:hover:-translate-y-8">
-                                <img class="object-cover object-center w-full rounded-t-xl lg:h-48 md:h-36" src="/assets/images/placeholders/neon-2.jpg" alt="blog"/>
-                                    <div class="px-6 py-8">
-                                        <h4 class="mt-4 text-2xl font-semibold text-neutral-600">
-                                            <span class="">Entry</span>
-                                        </h4>
-                                        <p class="mt-4 text-base font-normal text-gray-500 leading-relax">Install Tailwind CSS without any Javascript Framewrok locally with purgeCSS, enable the dark mode option, prefferences or class is upt to you.</p>
-                                    </div>
-                            </div>
-                        </div>
-                    </a>
-                    <div className='ml-6 flex items-center'>
-                        <button className='btn btn-link'>See All</button>
-                    </div>
-
+            <h2 className='text-3xl text-center font-bold mb-6'>My services</h2>
+            <div class="flex flex-wrap mx-auto md:flex-nowrap w-3/4">
+                {
+                    services.map(service => <HomepageServiceCard
+                        key={service._id}
+                        service={service}
+                    ></HomepageServiceCard>)
+                }
+                <div className='ml-6 flex items-center'>
+                    <Link to = '/services' className='btn btn-link text-lg font-semibold'>
+                        <button >See All</button>
+                        </Link>
                 </div>
-            </section>
+
+            </div>
+        </section>
     );
 };
 
